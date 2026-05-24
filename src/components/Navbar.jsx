20287adae61ba3a1bab2293a04b6b5960e7ae5dd
Daniel Quiguango <fiel2589@hotmail.com>
2026-05-24 18:32:54 -0500

    final changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -286,10 +286,10 @@ const Navbar = () => {
           ))}
         </ul>
             <a
-              href="https://kiwiowndaniel27.github.io/FreePass.com/#/tienda"
+              href="https://kiwiowndaniel27.github.io/Bakery_LaCr-me.com/#/tienda"
               className="text-black text-lg dark:text-white font-bold inline-block px-4 primary-btn duration-200 hover:text-amber-400 active:scale-95"
               onClick={handleClick}>
-              Tienda
+              Menú
             </a>
       </nav>
       
